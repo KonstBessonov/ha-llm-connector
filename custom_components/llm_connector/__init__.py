14584545f4860186d92763ecb31b4c7904c197e6
KonstBessonov <bessonov.ka@gmail.com>
2025-08-13 19:49:14 +0500

    Config flow support

--- custom_components/llm_connector/__init__.py
+++ custom_components/llm_connector/__init__.py
@@ -5,8 +5,12 @@ from .const import DOMAIN, STORAGE_VERSION, STORAGE_KEY
 from .services import async_setup_services
 from .chat_manager import ChatManager
 
-async def async_setup_entry(hass: HomeAssistant, entry):
+async def async_setup(hass: HomeAssistant, config) -> bool:
     """Настройка интеграции."""
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry):
+    """Настройка интеграции через config flow."""
     store = Store(hass, STORAGE_VERSION, STORAGE_KEY)
     chat_manager = ChatManager(store)
     await chat_manager.async_load()
@@ -22,6 +26,5 @@ async def async_setup_entry(hass: HomeAssistant, entry):
 
 async def async_unload_entry(hass: HomeAssistant, entry):
     """Выгрузка интеграции."""
-    # Очистка данных
     hass.data.pop(DOMAIN)
     return True
