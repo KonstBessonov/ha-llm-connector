1d43c2a1abc7318e8469f692c26564239a0e951d
KonstBessonov <bessonov.ka@gmail.com>
2025-08-14 15:56:22 +0500

    Fix response

--- custom_components/llm_connector/services.py
+++ custom_components/llm_connector/services.py
@@ -2,7 +2,7 @@
 import logging
 from typing import Any, Dict
 import voluptuous as vol
-from homeassistant.core import HomeAssistant, ServiceCall
+from homeassistant.core import HomeAssistant, ServiceCall, ServiceResponse, SupportResponse
 from homeassistant.helpers import config_validation as cv
 
 from .const import DOMAIN
